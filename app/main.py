363a6cca9c141e464ade0491e1a4887aa180d679
Livingstone143 <paramalla2004@gmail.com>
2024-09-19 04:06:19 +0530

    3

--- app/main.py
+++ app/main.py
@@ -13,6 +13,13 @@ def main():
     while True:
         try:
             buf, source = udp_socket.recvfrom(512)
+            rid = b"\x04\xd2"
+            rflags = b"\x80\x00"
+            qdcount = b"\x00\x01"
+            header = rid + rflags + qdcount + (b"\x00" * 6)
+            question = b"\x0ccodecrafters\x02io\x00\x00\x01\x00\x01"
+            response = header + question
+            response = b"\x04\xd2\x80" + (b"\x00" * 9)
     
             response = b""
             response = b"\x04\xd2\x80" + (b"\x00" * 9)
